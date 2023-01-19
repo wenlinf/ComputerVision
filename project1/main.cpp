98e4236d6a2d2d1e5bc17ef4353e71117cbf8d8b
Wenlin Fang <fang.wenl@northeastern.edu>
2023-01-19 11:41:23 -0500

    Initial Commit

---
+++ project1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  project1
+//
+//  Created by 方文琳 on 2023/1/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
